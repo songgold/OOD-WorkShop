97d975fc6eef72edb005b4f0cd3deb62ad48d39e
songgold.sap@turbo.co.th <songgold.sap@turbo.co.th>
2021-07-24 11:57:04 +0700

    1

--- src/test/java/GenerateIdServiceTest.java
+++ src/test/java/GenerateIdServiceTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GenerateIdServiceTest {
-  
+    @Test
+    @DisplayName("ต้องได้ id=XYZ7")
+    public void case01() {
+        GenerateIdService service = new GenerateIdService();
+        String id = service.getId();
+        assertEquals("XYZ7", id);
+    }
 }
