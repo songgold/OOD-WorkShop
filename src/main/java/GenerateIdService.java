f0f92c1e088ffda45c53b02c2c7bc67ac4c2a05c
songgold.sap@turbo.co.th <songgold.sap@turbo.co.th>
2021-07-24 11:56:01 +0700

    1

---
+++ src/main/java/GenerateIdService.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GenerateIdService {
+    public String getId() {
+        Random random = new Random();
+        int number = random.nextInt(10);
+        return  "XYZ" + number;
+    }
+}
